7300ca7b4ed7d1a2e862263bd7b0f69b2a3f81eb
jm <jm@server>
2015-07-16 22:41:43 +0200

    fixing logging clashes, see https://github.com/ufal/lindat-dspace/issues/136

--- src/main/java/fr/cines/eudat/log/Log.java
+++ src/main/java/fr/cines/eudat/log/Log.java
@@ -17,8 +17,18 @@ public class Log {
 	//pour activer le logger faire new Log();
 	//changer le chemin  vers le fichier de log
 	public Log(){
-            
-		_log4jXmlPath= "log4j.xml";
+
+		// imagine all dependencies would ship their own log4j property file
+		// and would require different parameters for log directory...
+		// however, we can do two small hacks to make it usable!
+
+		// do not even try to output to "/debug.log"
+		if ( null == System.getProperty("RP_LOG_DIR") ) {
+			return;
+		}
+
+		// do not use log4j.xml which might get loaded by default!!!
+		_log4jXmlPath= "b2safe-log4j.xml";
         // DOMConfigurator.configure(Log.class.getResource(_log4jXmlPath));
         DOMConfigurator.configure(this.getClass().getClassLoader().getResource(_log4jXmlPath));
 	}
